fd3a8e7b40b60097a5e3d2fe39097e4c9a003f00
jorgestefania1999-hub <jorgestefania1999@gmail.com>
2026-04-17 13:59:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -63,4 +63,8 @@ io.on("connection", (socket) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log("Servidor corriendo en puerto " + PORT);
+  fix: clean index
+  // update
+  
+  
 });
